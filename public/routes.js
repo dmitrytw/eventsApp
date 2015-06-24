4d56fa90152e0bfc0810c92ae48c7e95e38a3a97
dmitrytw <dmitry.tw@gmail.com>
2015-06-24 16:59:33 +0300

    Added fresh events route. Deleted unnecessary test routes.

--- public/routes.js
+++ public/routes.js
@@ -5,50 +5,34 @@ var app = angular.module('routes-module', ['ngRoute']);
 //configure routes
 app.config(function($routeProvider){
 	$routeProvider
-		//home page route
+		//Fresh events page route
 		.when('/', {
-			templateUrl: 'pages/home.html',
-			controller: 'mainController'
+			templateUrl: 'pages/fresh-events.html',
+			controller: 'FresheventsController as eventList'
 		})
 
-		// about page route
-		.when('/about', {
-			templateUrl: 'pages/about.html',
-			controller: 'aboutController'
-		})
-
+		//English clubs page route
 		.when('/engclubs', {
 			templateUrl: 'pages/events.html',
 			controller: 'EngclubsController as eventList'
 		})
 
+		//Mafia clubs page route
 		.when('/mafiaclubs', {
 			templateUrl: 'pages/events.html',
 			controller: 'MafiaclubsController as eventList'
 		})
 
+		//Coworkings page route
 		.when('/coworkings', {
 			templateUrl: 'pages/coworkings.html',
 			controller: 'CoworkingsController as eventList'
 		})
 
-		//contact page route
+		//Contact page route
 		.when('/contact', {
 			templateUrl: 'pages/contact.html',
 			controller: 'ContactformController'
 		});
 });
 
-//create controller and inject angular scope
-app.controller('mainController', function($scope){
-	//create a message to display in our view
-	$scope.message = 'Yaaha! Angular rocks at home!';
-});
-
-app.controller('aboutController', function($scope){
-	$scope.message = 'Oh! Look what i can! And you\'ll know more about me!';
-});
-
-app.controller('contactController', function($scope){
-	$scope.message = 'You can contact me on this page!';
-});
